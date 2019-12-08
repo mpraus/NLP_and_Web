ff64c1050ec5ee9223f58a57aae05ac702d09a7f
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-12-09 00:21:27 +0100

    added stuff

--- Milestone2/preprocess.py
+++ Milestone2/preprocess.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import spacy
+from spacy.attrs import LOWER, POS
+from spacy.matcher import Matcher
 
-df = pd.read_csv("evaluation_examples.csv", sep=',', header=None)
+
+df = pd.read_csv("evaluation_examples_backup.csv", sep=',', header=None)
 nlp = spacy.load('en_core_web_sm')
-df[0] = df[0].apply(lambda x: nlp(x))
+df["tokens"] = df[0].apply(lambda x: nlp(x))
+#df["as_array"] = df[0].apply(lambda x : x.to_array([LOWER, POS]))
 print(len(df[0]))
+matcher = Matcher(nlp.vocab)
+pattern = [{POS : {"NOT_IN" : list(("ADJ", "ADV"))}}]
+matcher.add("Adj,adv", None, pattern)
 for i in range(0, len(df[0])):
-	tokens = df[0].at[i]
-	#print(tokens)
-	for token in tokens:
-		if token.pos_ in ("ADV", "ADJ"):
-			tokens.remove(token)
-			#print(token)
+	tokens = nlp(df[0].at[i])
+	matches = matcher(tokens)
+	df["filtered"].at[i] = [tokens[start:end].text for match_id, start, end in matches]
+	# print(len(matches))
+	# print(set([tokens[start:end].text for match_id, start, end in matches]))
 print(df)
 print(df.columns)
